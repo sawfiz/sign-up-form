f4c63f5696c7f648c7e5621d8d35b15fbee299f6
sawfiz <dashu.up@gmail.com>
2022-10-06 13:18:53 +0800

    Update input styling

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# sign-up-form
-A project from Intermediate HTML and CSS course, the Odin Project
+# sign-up-for
+[Project: Sign-Up Form](https://www.theodinproject.com/lessons/node-path-intermediate-html-and-css-sign-up-form) from [Intermediate HTML and CSS](https://www.theodinproject.com/paths/full-stack-javascript/courses/intermediate-html-and-css) course, [the Odin Project](https://www.theodinproject.com/)
+
+## To-do
+- HTML based validation for `tel`
+- js based input validation
+
+## Key new concepts used
+
+### Assets
+- Crop SVG by changing `viewbox`
+- Use Webfont Generator at [Font Squirrel](https://www.fontsquirrel.com/tools/webfont-generator)
+
+### HTML
+- Input validation, `required`, `minlength`
+
+### CSS
+- `input:valid`, `input:invalid`
+- `outline: none`
+- `input::placeholder`
+- Use `position: relative` (parent), `position: abosolute` (child) to place elements
--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const pwd1El = document.querySelector("#password1");
-const pwd2El = document.querySelector("#password2");
-const submitEl = document.querySelector("button");
-const errorMsgEl = document.querySelector(".error-msg");
+const pwd1El = document.querySelector('#password1');
+const pwd2El = document.querySelector('#password2');
+const submitEl = document.querySelector('button');
+const errorMsgEl = document.querySelector('.error-msg');
 
-submitEl.addEventListener("click", (e) => {
-    if (!validatePasswords()) {
-        e.preventDefault();
-        errorMsgEl.removeAttribute("hidden");
-    }
+submitEl.addEventListener('click', (e) => {
+  if (!validatePasswords()) {
+    e.preventDefault();
+    errorMsgEl.removeAttribute('hidden');
+  }
 });
 
 function validatePasswords() {
-    return pwd2El.value === pwd1El.value ? true : false;
+  return pwd2El.value === pwd1El.value ? true : false;
 }
 
-pwd2El.addEventListener("input", () => {
-        errorMsgEl.setAttribute("hidden", true);
+pwd2El.addEventListener('input', () => {
+  errorMsgEl.setAttribute('hidden', true);
 });
